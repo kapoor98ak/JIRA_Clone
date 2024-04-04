7c09a16173b1f07575213447eb8a506f77c0c9a1
Abhishek Kapoor <ab210637@dal.ca>
2024-04-04 02:44:06 -0300

    Send Email success from Python Code

--- sendEmail.py
+++ sendEmail.py
@@ -4,14 +4,18 @@ import json
 lambda_client = boto3.client('lambda')
 
 def invoke_lambda_function(function_name, payload):
+    # This is an important step!!!
+    
     payload_bytes = json.dumps(payload).encode('utf-8')
 
     try:
+        print("Invoking Lambda!")
         response = lambda_client.invoke(
             FunctionName=function_name,
             InvocationType='RequestResponse',  # Can be 'Event' for asynchronous invocation
             Payload=payload_bytes
         )
+        print(response.__class__)
         return response
     except Exception as e:
         print(f"Error invoking Lambda function: {e}")
@@ -19,12 +23,13 @@ def invoke_lambda_function(function_name, payload):
 if __name__ == "__main__":
     
     # Put the Lambda Function's Name here!!
-    
-    function_name = 'test-lambda'
+
+    # function_name = 'test-lambda'
+    function_name = 'emailSenderLambdaFunction'
     
     payload = {
-        'Subject': 'EMail Subject',
-        'Body': 'EMail Body'
+        'Subject': 'EMail Subject 1',
+        'Body': 'EMail Body 2'
     }
     
     # Invoke the Lambda function
